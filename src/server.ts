61a7e25e94f9c5152794664b02a2966d2c927108
Izabella Alves <iap21010705@gmail.com>
2025-11-07 21:07:31 -0300

    chore: corrige erro de build

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,8 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
+    res.sendStatus(204);
+    return;
   }
 
   next();
